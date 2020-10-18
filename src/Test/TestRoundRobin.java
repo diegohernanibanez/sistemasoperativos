0ee4fe0153aa3c50e456f8a24af4742ae39c34f4
Matias Velez <punisher-1213@hotmail.com>
2020-10-18 15:48:06 -0300

    **Se modifica FIFO y roundRobin

--- src/Test/TestRoundRobin.java
+++ src/Test/TestRoundRobin.java
@@ -20,16 +20,19 @@ public class TestRoundRobin {
 
 		admProcesamiento admP1 = new admProcesamiento(20, 38);
 
-		admP1.agregarProceso("P1", 1, 3, 2, 4, Prioridad.Media);
-		admP1.agregarProceso("P2", 2, 2, 3, 2, Prioridad.Baja);
-		admP1.agregarProceso("P3", 4, 4, 2, 1, Prioridad.Alta);
-		admP1.agregarProceso("P4", 6, 1, 1, 1, Prioridad.Media);
+		admP1.agregarProceso("P1", 1, 4, 2, 6, Prioridad.Alta);
+		admP1.agregarProceso("P2", 2, 4, 3, 2, Prioridad.Baja);
+		admP1.agregarProceso("P3", 3, 2, 2, 5, Prioridad.Media);
+		admP1.agregarProceso("P4", 4, 3, 5, 2, Prioridad.Baja);
+		admP1.agregarProceso("P5", 5, 5, 4, 1, Prioridad.Media);
+		admP1.agregarProceso("P6", 6, 1, 4, 5, Prioridad.Alta);
+		admP1.agregarProceso("P7", 7, 4, 5, 2, Prioridad.Alta);
 
-		System.out.println("----------- Planificador RoundRobin q=1 -----------");
-		System.out.println(admP1.mostrarPlanificador(admP1.planificarRoundRobin(2)));
+		System.out.println("----------- Planificador RoundRobin q=3 -----------");
+		System.out.println(admP1.mostrarPlanificador(admP1.planificarRoundRobin(3)));
 		System.out.println(admP1.mostrarProcesos() + "\n-> hay 1 procesador" + "\n-> E/S Se realiza en paralelo\n");
 		
-		System.out.println("----------- Resultados RoundRobin q=1 -----------");
+		System.out.println("----------- Resultados RoundRobin q=3 -----------");
 		admTablaProcesos admTP1 = new admTablaProcesos(admP1);
 		System.out.println(admTP1.mostrarResultados(admP1.planificarRoundRobin(2)));
 
